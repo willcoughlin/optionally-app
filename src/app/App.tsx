86663dfd1417a7c057a5c02244ea752595fb153f
Will Coughlin <willcoughlin@gmail.com>
2021-01-16 15:56:28 -0600

    Create react-navigation starting point

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { registerRootComponent } from 'expo';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
 
-const App = () => (
-  <View style={styles.container}>
-    <Text>Open up App.tsx to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
-);
+const Stack = createStackNavigator();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => <NavigationContainer>
+  <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomeScreen} />
+  </Stack.Navigator>
+</NavigationContainer>;
 
 registerRootComponent(App);
 export default App;
---
+++ src/app/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import mainStyle from '../styles/main-style';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => (
+    <View style={mainStyle.container}>
+        <Text>Home Screen</Text>
+    </View>
+);
+
+export default HomeScreen;
