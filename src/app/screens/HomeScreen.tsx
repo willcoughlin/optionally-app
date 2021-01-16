632714c28507432c901ff0bf9b895d9e1c6831ea
Will Coughlin <willcoughlin@gmail.com>
2021-01-16 17:31:15 -0600

    Begin main layout

--- src/app/screens/HomeScreen.tsx
+++ src/app/screens/HomeScreen.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import { StyleProp, View, ViewStyle } from 'react-native';
+import { Button, Headline, ProgressBar, Subheading, TextInput, Title } from 'react-native-paper';
 import mainStyle from '../styles/main-style';
-import { View, Text } from 'react-native';
 
-const HomeScreen = () => (
-    <View style={mainStyle.container}>
-        <Text>Home Screen</Text>
-    </View>
+const BottomStickyView: FunctionComponent = ({ children }) => (
+  <View style={{ 
+      position: 'absolute', 
+      left: 0,
+      right: 0,
+      bottom: 0,
+      // borderColor: 'black',
+      // borderWidth: 1,
+      width: '100%',
+      margin: 20
+    }}>
+    {children}
+  </View>
 );
+
+const HomeScreen = () => {
+  const [symbolInput, setSymbolInput] = React.useState('');
+
+  return (  
+    <View style={mainStyle.container}>
+      <Headline>First things first</Headline>
+      <Subheading>Choose an underlying stock/ETF</Subheading>
+      <TextInput 
+        label="Symbol" 
+        mode="outlined"
+        value={symbolInput}
+        onChangeText={text => setSymbolInput(text)} />
+      
+      <BottomStickyView>
+        <ProgressBar progress={0.5} />
+        <Button>
+          Next
+        </Button>
+      </BottomStickyView>
+    </View>
+  );
+};
 
 export default HomeScreen;
--- src/app/styles/main-style.ts
+++ src/app/styles/main-style.ts
@@ -1,12 +1,14 @@
 import { StyleSheet } from 'react-native';
 
 const mainStyle = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20
+  },
 });
 
 export default mainStyle;
