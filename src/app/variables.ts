38b485acfc9b8128e78810bfe3afb6e972691845
Will Coughlin <willcoughlin@gmail.com>
2021-02-24 21:15:50 -0600

    Update local gql server url

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -5,7 +5,7 @@ interface EnvironmentVariables {
 const Variables: EnvironmentVariables = __DEV__
   ? ({
     // Dev variables
-    serverUrl: 'http://192.168.1.106:3000/graphql'
+    serverUrl: 'http://192.168.1.124:3000/graphql'
   }) 
   : ({
     // Prod variables 
