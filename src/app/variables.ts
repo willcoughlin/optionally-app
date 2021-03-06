f980f38a601c76ac7a2d49daaff50d52e0678358
Will Coughlin <willcoughlin@gmail.com>
2021-03-06 13:48:13 -0600

    Fix issue with prod serverUrl variable

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -5,11 +5,12 @@ interface EnvironmentVariables {
 const Variables: EnvironmentVariables = __DEV__
   ? ({
     // Dev variables
-    serverUrl: 'http://192.168.1.124:3000/graphql'
+    // serverUrl: 'http://192.168.1.124:3000/graphql'
+    serverUrl: 'https://api.optionally.app/graphql'
   }) 
   : ({
     // Prod variables 
-    serverUrl: 'https://api.optionally.com/graphql'
+    serverUrl: 'https://api.optionally.app/graphql'
   });
 
 export default Variables;
